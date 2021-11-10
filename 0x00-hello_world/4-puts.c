957705a7115327f92bf4945c41b90aaa6967f816
WangechiMurimi <murimiwangechi@gmail.com>
2021-11-10 19:20:13 +0300

    Adding quotes to strings

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 int main(void)
 {
   /*Initializaing strings*/
-  char str1[] = "Programming is like building a multilingual puzzle \n";
+  char str1[] = "\"Programming is like building a multilingual puzzle \n";
 
   puts(str1);
 
